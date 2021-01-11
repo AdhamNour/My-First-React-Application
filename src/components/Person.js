a5587981da60ae3181a399a8c1d19de6d441b9cb
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-11 15:05:58 +0200

    first commit

---
+++ src/components/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Person = (props) =>{
+    return (
+        <div>
+            Hello two
+            <p>{props.num}</p>
+        </div>
+    );
+}
+
+export default Person;
