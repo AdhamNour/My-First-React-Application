a39da4963c138d2c2f9c7f0eab2b69d087d55e1f
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-12 14:59:07 +0200

    finished video 5 module 6

--- src/containers/App.js
+++ src/containers/App.js
@@ -68,6 +68,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit 
+          title={this.props.appTitle}
           persons={this.state.persons} 
           showPersons={this.state.showPersons}
           buttonClick={this.togglePersonsHandler}
