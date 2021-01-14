6119d6c8530565c156407e7b4672cbd6ee14e818
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-14 10:44:25 +0200

    finished video 13 in module 7

--- src/components/cockpit/cockpit.js
+++ src/components/cockpit/cockpit.js
@@ -4,10 +4,11 @@ import classes from "./Cockpit.css"
 const Cockpit = (props) => {
   useEffect(() =>{
     console.log("Cockpit.js useEffect that depends on props.persons called");
-    setTimeout(() =>{
+    const timer = setTimeout(() =>{
       alert("Saved data");
     },1000)
     return () => {
+      clearTimeout(timer)
       console.log("Cockpit.js cleanup works")
     }
   },[props.persons]);
