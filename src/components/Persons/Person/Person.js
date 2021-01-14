a43a53f4bc98aefc30eabe5a71903374d3ad5cb6
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-14 14:41:39 +0200

    finished video 19 from module 7

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
 import classes from "./Person.css";
+import Aux from "../../../hoc/Auxiliry"
 
 class Person extends Component {
   render() {
     console.log("person.js is rendering ");
 
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
@@ -17,7 +18,7 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Aux>
     );
   }
 }
